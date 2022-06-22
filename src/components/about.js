6f9b7d38f4af1907e5cd53346935f9fc667097bd
CovJus01 <covjus01@gmail.com>
2022-06-21 23:50:31 -0400

    Created intro component and baseline layout

--- src/components/about.js
+++ src/components/about.js
@@ -2,7 +2,28 @@ import React from "react";
 
 //Body
 const About = () => {
-  return <div className="about"></div>;
+  return (
+    <div className="about">
+      <div className="content-wrap">
+        <div>
+          <h1>About Me</h1>
+        </div>
+        <div>
+          <p className="">
+            Currently Exploring and Learning technologies related to
+            web-development, applications involving both frontend and backend
+            frameworks and tools
+          </p>
+          <p className="">
+            3rd year Computer Engineering student at McMaster University
+          </p>
+          <p className="">
+            3rd year Computer Engineering student at McMaster University
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/introduction.js
+++ src/components/introduction.js
@@ -2,7 +2,18 @@ import React from "react";
 
 //Body
 const Introduction = () => {
-  return <div className="introduction"></div>;
+  return (
+    <div className="introduction">
+      <div className="content-wrap">
+        <div>
+          <h1>Justin Covach</h1>
+          <p className="right-align">
+            3rd year Computer Engineering student at McMaster University
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Introduction;
